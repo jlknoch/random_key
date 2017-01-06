bd93181ee4cf1a67e5f4d0f5df9837a7134ba48b
Jan-Lukas Knoch <jl_knoch@protonmail.com>
2017-01-06 03:46:12 +0100

    Add files via upload

---
+++ random.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main(){
+
+	char chars[] = "qwert_zuiopas_df_ghjklyxcvbnmQWERT_ZUIO_PASDFGHJK_LYXCVBNM1_234567890_";
+	string rndchars;
+	int length, rndnum;
+
+	cout << "Enter length of password: ";
+	cin >> length;
+
+	srand(time(NULL));
+
+	for(int i = 1; i <= length; i++)
+	{
+	rndnum = rand() % 70 +1;	
+	cout << chars[rndnum];
+	}
+	
+	cout << "\n";
+	return 0;
+}
---
+++ randomkeys.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+	
+	int flength, length, rndnum;
+	
+	cout << "How much random keys do you want to generate? ";
+	cin >> flength;	
+	cout << "Enter length of password: ";
+	cin >> length;
+	
+	srand(time(NULL));
+	cout << "---STARTLINE---" << endl;
+
+	for(int i = 1; i <= flength; i++)
+	{
+	char chars[] = "qwert_zuiopas_df_ghjklyxcvbnmQWERT_ZUIO_PASDFGHJK_LYXCVBNM1_234567890_";
+	
+
+	for(int i = 1; i <= length; i++)
+	{
+	rndnum = rand() % 70 +1;
+	cout << chars[rndnum];
+	}	
+	cout << endl;
+
+	}
+	
+	cout << "---ENDLINE---" << endl;
+	return 0;	
+}
+
